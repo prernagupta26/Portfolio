560c687118c6f2a41e5ed30397639ee39d045627
Prerna Gupta <guptaprerna2607@gmail.com>
2023-07-20 22:15:20 +0530

    change

---
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,78 @@
+import React, { useEffect } from "react";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
+import "./AboutMe.css";
+
+export default function AboutMe(props) {
+  
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const SCREEN_CONSTSANTS = {
+    description:
+      "Highly motivated and technically skilled individual with a strong foundation in Java, C++, object-oriented programming, and data structures. Eager to apply my knowledge, learn new technologies, and make a positive impact in the field of technology. Seeking a position in an engineering environment where I could add my knowledge and skills for the better productivity of the organization and at the same time enhance my knowledge and develop more skills",
+    highlights: {
+      bullets: [
+        "Java",
+        "SpringBoot, Microservices",
+        "c++",
+        "Data Structures and Algorithms",
+        "Object Oriented Programming",
+        "Operating Systems",
+        
+      ],
+      heading: "Skills:",
+    },
+  };
+  const renderHighlight = () => {
+    return SCREEN_CONSTSANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+
+  return (
+    <div
+      className="about-me-container screen-container fade-in"
+      id={props.id || ""}
+    >
+      <div className="about-me-parent">
+        <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {SCREEN_CONSTSANTS.description}
+            </span>
+            <div className="about-me-highlights">
+              <div className="highlight-heading">
+                <span>{SCREEN_CONSTSANTS.highlights.heading}</span>
+              </div>
+              {renderHighlight()}
+            </div>
+            <div className="about-me-options">
+              {/* <button
+                className="btn primary-btn" onClick={() => ScrollService.scrollHandler.scrollToHireMe()} >
+                {" "}
+                Hire Me{" "}
+              </button> */}
+              <a href="resume.pdf" download="Prerna Gupta.pdf">
+                <button className="btn highlighted-btn">Get Resume</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
